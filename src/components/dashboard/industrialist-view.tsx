22403af99ab40a68ccb789e3a4ae853d204d6aa8
Nallana Raghavendra <Raghavendranallana@gmail.com>
2025-07-13 18:33:45 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/dashboard/industrialist-view.tsx
+++ src/components/dashboard/industrialist-view.tsx
@@ -6,7 +6,7 @@ import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
