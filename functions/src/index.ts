6358d7860e7b4a3c5727a94426aa2ed3afd15ef9
Nallana Raghavendra <Raghavendranallana@gmail.com>
2025-07-15 03:54:30 +0000

    I have nothing in transporters home page. Update it

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,7 @@
 /**
  * Import function triggers from their respective submodules:
  *
- * import {onCall} from "firebase-functions/v2/https";
+ * import {onCall} from "firebase-functions/v2/h";
  * import {onDocumentWritten} from "firebase-functions/v2/firestore";
  *
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
